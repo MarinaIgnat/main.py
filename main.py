35e59cdbef36735cdf892f2d166cce5289ac9f8b
MarinaIgnat <9702m@mail.ru>
2024-11-16 18:20:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#1st program
+print(9**0.5*5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+print('2*2+2')
+a = 2*2+2
+print('2*(2+2)')
+b = 2*(2+2)
+print(a == b)
+
+#4st program
+a = 123.456
+print(int((float('123.456'))*10)%10)
